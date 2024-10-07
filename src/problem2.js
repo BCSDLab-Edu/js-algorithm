0c3ad398cdfe9d78bfb665bfa2f47ef88141017e
asa9874 <pjkpjk1129@gmail.com>
2024-10-07 23:41:55 +0900

    feet: 문제3

--- src/problem2.js
+++ src/problem2.js
@@ -1,8 +1,8 @@
 function problem2(cryptogram) {
-  var answer=[];//스택처럼
-  cryptogram.split('').forEach( text =>{
-    if(answer.length===0) answer.push(text);
-    else if(answer[answer.length - 1]===text) answer.pop();
+  var answer = [];//스택처럼
+  cryptogram.split('').forEach(text => {
+    if (answer.length === 0) answer.push(text);
+    else if (answer[answer.length - 1] === text) answer.pop();
     else answer.push(text);
   })
 
--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,12 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  let now = 1;
+  while (now !== number + 1) {
+    now.toString().split('').forEach(num => {
+      if (num !== '0' && num % 3 === 0) answer++;
+    })
+    now++;
+  }
   return answer;
 }
 
