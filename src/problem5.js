3f81ea28a2ce9ab810cce400cc90931440908fff
asa9874 <pjkpjk1129@gmail.com>
2024-10-08 00:13:59 +0900

    Feat:문제5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,11 @@
 function problem5(money) {
-  var answer;
+  var answer=[];
+  const bills=[50000,10000,5000,1000,500,100,50,10,1];
+  let nowMoney=money;
+  bills.forEach(bill => {
+    answer.push(Math.floor(nowMoney/bill));
+    nowMoney%=bill;
+  });
   return answer;
 }
 
