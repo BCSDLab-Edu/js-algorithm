30b5e51459aed27e2a923b3a44205beed62059f0
박성주 <psj001002@naver.com>
2025-10-04 14:26:43 +0900

    1~3문제 해결

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,30 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) {
+    return -1;
+  }
+  const pobiMax = Math.max(maxValue(String(pobi[0])), maxValue(String(pobi[1])));
+  const crongMax = Math.max(maxValue(String(crong[0])), maxValue(String(crong[1])));
+
+  if(pobiMax > crongMax){
+    return 1;
+  } else if(pobiMax < crongMax){
+    return 2;
+  } else {
+    return 0;
+  }
+  
 }
 
+function maxValue(page){
+  let sum = 0;
+  let mul = 1;
+  const pageStr = page.split("");
+  for(let i of pageStr){
+    sum += Number(i);
+    mul *= Number(i);
+  }
+  return Math.max(sum, mul);
+}
+
+  
 module.exports = problem1;
--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,14 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const stack = [];
+  let secret = cryptogram.split("");
+  for( let i = 0; i < secret.length; i++){
+    stack.push(secret[i]);
+    if(stack.length >= 2 && stack[stack.length - 1] === stack[stack.length - 2]){
+      stack.pop();
+      stack.pop();
+    }
+  }
+  return stack.join("");
 }
 
 module.exports = problem2;
