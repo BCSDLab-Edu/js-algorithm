05b891f36aeb12d624479acd88c16a8ff250ce50
박성주 <psj001002@naver.com>
2025-10-05 14:15:47 +0900

    4-5

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,24 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let result = '';
+
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i];
+    const charCode = char.charCodeAt(0);
+
+    if (charCode >= 65 && charCode <= 90) {
+      const newCharCode = 90 + 65 - charCode;
+      result += String.fromCharCode(newCharCode);
+    } 
+    else if (charCode >= 97 && charCode <= 122) {
+      const newCharCode = 122 + 97 - charCode;
+      result += String.fromCharCode(newCharCode);
+    } 
+    else {
+      result += char;
+    }
+  }
+  return result;
 }
+
 
 module.exports = problem4;
