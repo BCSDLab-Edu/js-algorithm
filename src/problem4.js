4fcd92eb7d0bd0a2678e110d5645805209c6ade9
DonghyeonLee03 <leedh0921@naver.com>
2025-10-05 22:04:57 +0900

    문제4 해결

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,23 @@
 function problem4(word) {
   var answer;
+  answer = '';
+    
+  var code = [0, 0];
+  var startWord = ['A'.charCodeAt(), 'a'.charCodeAt()];
+  var endWord = ['Z'.charCodeAt(), 'z'.charCodeAt()];
+    
+  for (var loc = 0; loc < word.length; loc++){
+    for (var upDown = 0; upDown < code.length; upDown++){
+      code[upDown] = startWord[upDown] + endWord[upDown] - word[loc].charCodeAt(); 
+      if (code[upDown] <= endWord[upDown] && code[upDown] >= startWord[upDown]){
+        answer += String.fromCharCode(code[upDown]);
+        break;
+      }
+      if (upDown == 1){
+        answer += word[loc];
+      }
+    }
+  }
   return answer;
 }
 
